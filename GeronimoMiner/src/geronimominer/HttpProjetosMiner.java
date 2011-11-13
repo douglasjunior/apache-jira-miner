fef3ec23c7adeb093190f8137fcef291b845edf3
nassifrroma@gmail.com <nassifrroma@gmail.com@8533918b-2804-2cb3-6c4c-626e76a539af>
2011-11-13 23:39:06 +0000

    removidas importacoes nao utilizadas

--- GeronimoMiner/src/geronimominer/HttpProjetosMiner.java
+++ GeronimoMiner/src/geronimominer/HttpProjetosMiner.java
@@ -8,7 +8,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.URL;
 import java.util.Date;
